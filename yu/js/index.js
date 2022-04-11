f0e99ae3c427479a00de4fa52a7ac87386d68b48
amy89011187 <98808531+amy89011187@users.noreply.github.com>
2022-04-11 17:50:46 +0800

    Add files via upload

---
+++ yu/js/index.js
@@ -0,0 +1,97 @@
+
+    $(window).on("load", function() { // makes sure the whole site is loaded
+      //preloader
+      $("#status").fadeOut(1800); 
+      $("#preloader").delay(1500).fadeOut("slow"); 
+      $(document).masonry();    
+    });
+
+var menubtn = document.getElementById('menubtn');
+var closebtn =document.getElementById('closebtn');
+var navbox =document.getElementById('navbox');
+$(document).ready(function() {
+    $('#menubtn').click(function() {
+        // $('#navbox').fadeIn(500);
+        $('#menubtn').hide(1000);
+        $('#navbox').css({"display":"block"}),
+        $('#navbox').animate({
+            height:'100%',
+            width: '15%',
+        })
+      });
+ $('#closebtn').click(function() {
+    $('#menubtn').show(400);
+    $('#navbox').css({"display":"none"}),
+    $('#navbox').animate({
+        height:'0',
+        width: '0',
+    })
+  });
+});
+
+// menubtn.onclick=function(){
+//     navbox.style.display="block";
+//     menubtn.style.display="none";
+// }
+// closebtn.onclick=function(){
+//     navbox.style.display="none";
+//     menubtn.style.display="block";
+// }
+
+var mySwiper = new Swiper('.swiper-container', {//初始化Swiper
+    autoplay: {//自動切換
+        delay: 15000,
+        stopOnLastSlide: false,
+        disableOnInteraction: false,
+    },
+        navigation: {//前進後退
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {//分頁器
+        el: '.swiper-pagination',
+        clickable :true,
+    },
+        loop : true,//循環
+    })
+
+  //active menu
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+   
+        $('a').each(function () {
+          $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+   
+        var target = this.hash;
+        $target = $(target);
+        $('html, body').stop().animate({
+          'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+          window.location.hash = target;
+          $(document).on("scroll", onScroll);
+        });
+      });
+      //scroll js
+    smoothScroll.init({
+        selector: '[data-scroll]',
+        selectorHeader: '[data-scroll-header]', 
+        speed: 500, 
+        easing: 'easeInOutCubic', 
+        updateURL: true, 
+        offset: 0,
+        callback: function ( toggle, anchor ) {}
+      });
+      //Top
+      $(document).ready(function() {
+
+        $('.top a').click(function(event) {
+         event.preventDefault(); //默認動作
+         $('html,body').animate({
+           scrollTop: 0
+         }, 1000);
+        });
+       
+       });
